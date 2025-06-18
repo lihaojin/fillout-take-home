459d05cf778f62f5558f9fb46225f21e9f40fe40
lihaojin <philpowa398@gmail.com>
2025-06-18 16:04:54 -0400

    fix: Fix window undefined

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 
 function useIsMobile(breakpoint = 768) {
-  const [isMobile, setIsMobile] = useState(
-    () => window?.innerWidth <= breakpoint
-  );
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window?.innerWidth <= breakpoint);
+      if (typeof window !== undefined) {
+        setIsMobile(window.innerWidth <= breakpoint);
+      }
     };
 
-    window?.addEventListener("resize", handleResize);
-    return () => window?.removeEventListener("resize", handleResize);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [breakpoint]);
 
   return isMobile;
